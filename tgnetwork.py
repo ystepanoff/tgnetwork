b84d25254cb6f20d3d8fc4a12c8afe4fb354323f
ystepanoff <ystepanoff@protonmail.com>
2021-07-11 03:25:28 +0100

    Buling Telegram channels network using BFS with limited tree height.

--- tgnetwork.py
+++ tgnetwork.py
@@ -1,5 +1,7 @@
-from discover import Observer
+from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
 from configparser import ConfigParser
+import csv
 
 config = ConfigParser()
 config.read('tgnetwork.config')
@@ -8,13 +10,57 @@ session = config['telegram']['session']
 api_id = config['telegram']['api_id']
 api_hash = config['telegram']['api_hash']
 
-channels = ['doktorvladi']
+channels_meta = {
+    'marinelepen': {
+        'name': 'Marine Le Pen',
+        'depth': 1,
+        'parent': None,
+        'total': 1
+    }
+}
+channels = ['marinelepen']
+max_depth = 10
+out_csv = 'marielepen.csv'
 
 if __name__ == '__main__':
-    observer = Observer(session, api_id, api_hash)
-    for channel in channels:
-        for message in observer.iter_messages(channel):
-            if message.forward:
-                print(message.forward.original_fwd)
-                print('---')
-                print(message)
+    with TelegramClient(session, api_id, api_hash) as client:
+        for ch in channels:
+            if channels_meta[ch]['depth'] >= max_depth:
+                continue
+            for message in client.iter_messages(ch):
+                if message.forward:
+                    forward = message.forward
+                    if forward.from_id:
+                        try:
+                            #client(JoinChannelRequest(forward.from_id.channel_id))
+                            channel = client.get_entity(forward.from_id.channel_id)
+                            if channel.username:
+                                if not channel.username in channels:
+                                    #print(channel)
+                                    channels.append(channel.username)
+                                    channels_meta[channel.username] = {
+                                        'name': channel.title,
+                                        'depth': channels_meta[ch]['depth'] + 1,
+                                        'parent': ch,
+                                        'total': 1
+                                    }
+                                    print(channels_meta)
+                                    print('----------------------')
+                                else:
+                                    channels_meta[channel.username]['total'] += 1
+                        except AttributeError:
+                            pass
+                        except:
+                            print('Error: ', forward.from_id)
+    with open(out_csv, 'wt') as file:
+        cw = csv.writer(file)
+        cw.writerow(['handle', 'title', 'depth', 'parent', 'total'])
+        for ch in channels:
+            row = [
+                ch,
+                channels_meta[ch]['title'],
+                channels_meta[ch]['depth'],
+                channels_meta[ch]['parent'],
+                channels_meta[ch]['total']
+            ]
+            cw.writerow(row)
